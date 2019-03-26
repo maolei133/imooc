4dfc0de73fa8aa17419a28b159b5ccd1b13b5aee
maolei133 <maolei133@163.com>
2019-03-26 23:53:52 +0800

    netty的websocket服务

---
+++ muxin/hello-netty/src/main/java/com/maolei/hello/netty/websocket/WSServer.java
@@ -0,0 +1,29 @@
+package com.maolei.hello.netty.websocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class WSServer {
+
+	public static void main(String[] args) throws Exception {
+		EventLoopGroup master = new NioEventLoopGroup();
+		EventLoopGroup slave = new NioEventLoopGroup();
+
+		try {
+			ServerBootstrap server = new ServerBootstrap();
+			server.group(master, slave).channel(NioServerSocketChannel.class).childHandler(new WSServerInitializer());
+
+			ChannelFuture future = server.bind(8088).sync();
+
+			future.channel().closeFuture().sync();
+		} finally {
+			master.shutdownGracefully();
+			slave.shutdownGracefully();
+		}
+
+	}
+
+}
